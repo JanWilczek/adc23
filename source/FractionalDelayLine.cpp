d33ecc2c508cd4fb3bf864f4cc4d167323bd10ca
Jan Wilczek <janekw@vp.pl>
2023-10-14 10:02:21 +0200

    Removed assertions.

--- source/FractionalDelayLine.cpp
+++ source/FractionalDelayLine.cpp
@@ -1,38 +1,33 @@
+#include "FractionalDelayLine.h"
 #include <algorithm>
 #include <array>
-#include <vector>
-#include <cmath>
 #include <cassert>
-#include "FractionalDelayLine.h"
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
 namespace DL {
 
 float FractionalDelayLine::popSample() {
-    assert(delay >= 0.f);
-    assert(delay < static_cast<float>(buffer.size()));
-    auto readHead = writeHead - 1 - delay;
-    if (readHead < 0) {
+  auto readHead = writeHead - 1 - delay;
+  if (readHead < 0) {
     readHead += std::ssize(buffer);
-    }
-    auto truncatedReadHead = static_cast<int>(std::floor(readHead));
-    auto truncatedReadHeadPlusOne = static_cast<int>(std::ceil(readHead)); // truncatedReadHead + 1; 
-    const auto truncatedReadHeadWeight =
-        std::abs(readHead - truncatedReadHeadPlusOne);
-    const auto truncatedReadHeadPlusOneWeight =
-        std::abs(readHead - truncatedReadHead);
-    if (truncatedReadHeadPlusOne >= std::ssize(buffer)) {
-        truncatedReadHeadPlusOne -= std::ssize(buffer);
-    }
-    assert(0 <= truncatedReadHead);
-    assert(0 <= truncatedReadHeadPlusOne);
-    assert(truncatedReadHead < std::ssize(buffer));
-    assert(truncatedReadHeadPlusOne < std::ssize(buffer));
-    const auto outputSample =
-        truncatedReadHeadWeight * buffer[truncatedReadHead] +
-        truncatedReadHeadPlusOneWeight * buffer[truncatedReadHeadPlusOne];
-    return outputSample;
+  }
+  auto truncatedReadHead = static_cast<int>(std::floor(readHead));
+  auto truncatedReadHeadPlusOne =
+      static_cast<int>(std::ceil(readHead)); // truncatedReadHead + 1;
+  const auto truncatedReadHeadWeight =
+      std::abs(readHead - truncatedReadHeadPlusOne);
+  const auto truncatedReadHeadPlusOneWeight =
+      std::abs(readHead - truncatedReadHead);
+  if (truncatedReadHeadPlusOne >= std::ssize(buffer)) {
+    truncatedReadHeadPlusOne -= std::ssize(buffer);
+  }
+  const auto outputSample =
+      truncatedReadHeadWeight * buffer[truncatedReadHead] +
+      truncatedReadHeadPlusOneWeight * buffer[truncatedReadHeadPlusOne];
+  return outputSample;
 }
 
 void FractionalDelayLine::pushSample(float inputSample) {
@@ -41,8 +36,6 @@ void FractionalDelayLine::pushSample(float inputSample) {
   writeHead %= std::ssize(buffer);
 }
 
-void FractionalDelayLine::setDelay(float newDelay) {
-  delay = newDelay;
-}
+void FractionalDelayLine::setDelay(float newDelay) { delay = newDelay; }
 
-}  // namespace DL
+} // namespace DL
