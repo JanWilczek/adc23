7502df80dda7f2811f33be2923f26552494468e4
Jan Wilczek <janekw@vp.pl>
2023-10-16 12:39:12 +0200

    End-to-end Python pipeline.

--- test/source/OutputTest.cpp
+++ test/source/OutputTest.cpp
@@ -10,16 +10,16 @@ TEST(OutputTest, GenerateSine) {
   int numChannels = 1;
   buffer.resize(numChannels);
 
-  int numSamplesPerChannel = 100000;
+  int numSamplesPerChannel = 100;
   buffer[0].resize(numSamplesPerChannel);
 
   float sampleRate = 44100.f;
   float frequency = 440.f;
 
   for (int i = 0; i < numSamplesPerChannel; i++) {
-    float sample = std::sin(2. * std::numbers::pi_v<float> *
-                            ((float)i / sampleRate) * frequency);
-    buffer[0][i] = sample * 0.5;
+    float sample = std::sin(2. * std::numbers::pi_v<float> * frequency *
+                            static_cast<float>(i) / sampleRate);
+    buffer[0][i] = sample * 0.5f;
   }
 
   ASSERT_TRUE(audioFile.setAudioBuffer(buffer));
--- test/source/test_and_plot.py
+++ test/source/test_and_plot.py
@@ -1,13 +1,22 @@
 import os
 import sys
 from pathlib import Path
+import matplotlib.pyplot as plt
+import soundfile as sf
 
 
 def main():
     current_dir_path = Path(os.path.dirname(os.path.abspath(sys.argv[0])))
-    root_repo_path = current_dir_path / '..' / '..'
+    root_repo_path = current_dir_path.parents[1]
     print(str(root_repo_path))
-    ret = os.system(f'cd {root_repo_path} && cmake -S . -B build && cmake --build build && .\\build\\test\Debug\ExamplesTest.exe --gtest_filter="OutputTest.*"')
+    test_executable_path = Path('build') / 'test' / 'Debug' / 'ExamplesTest.exe'
+    ret = os.system(f'cd {root_repo_path} && cmake -S . -B build && cmake --build build && {test_executable_path} --gtest_filter="OutputTest.*"')
+
+    data, fs = sf.read(root_repo_path / 'sine.wav')
+
+    plt.figure()
+    plt.plot(data)
+    plt.show()
 
 
 if __name__ == '__main__':
